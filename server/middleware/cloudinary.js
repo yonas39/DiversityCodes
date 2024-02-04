ad409f7bd1e69b16948c897309242256fdabddf4
JoseDiazCodes <josediazjr.a@gmail.com>
2024-02-04 17:31:46 -0500

    added and refractored the code

---
+++ server/middleware/cloudinary.js
@@ -0,0 +1,5 @@
+/**
+ * This function uploads an image to Cloudinary.
+ * @param {string} imagePath - The path of the image to be uploaded.
+ * @returns {Promise<string>} - A promise that resolves with the URL of the uploaded image.
+ */
