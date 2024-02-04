353c9699c528dbfd81d322534a6fc0128f272d49
Nisaury Amador Tejeda <namadortejed2021@fau.edu>
2024-02-03 21:15:21 -0500

    update to use nodemon

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('API is working properly!');
+  res.send('API is working properly! Test');
 });
 
 module.exports = router;
