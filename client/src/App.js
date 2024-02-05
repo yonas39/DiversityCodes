13d36af2c2af5747dde21c70e42e01574833db1b
Emily Flores <e_floresrodriguez@massbay.edu>
2024-02-04 20:32:56 -0500

    fixed unmerged file paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React from "react"
 import "./App.css"
 import ExamView from "./pages/ExamView"
@@ -10,25 +11,26 @@ function App() {
 			<Navbar /> {/* Render Navbar */}
 			<ExamView />
 =======
+=======
+import React from "react"
+>>>>>>> d07e010 (testing)
 import "./App.css"
-import { useApi } from "./hooks/use-api"
+import ExamView from "./pages/ExamView"
+import Navbar from "./components/NavBar" // Import Navbar // Import Navbar
 
 function App() {
-	const { response, error } = useApi({ path: "exams" })
-
-	console.log("response:", response)
-
-	if (error) {
-		return <div>Error: {JSON.stringify(error)}</div>
-	}
-
 	return (
 		<div className="App">
+<<<<<<< HEAD
 			<header className="App-header">
 				{Array.isArray(response) &&
 					response.map((exam, index) => <p key={index}>{exam.patientId}</p>)}
 			</header>
 >>>>>>> 381f817 (fixed repo)
+=======
+			<Navbar /> {/* Render Navbar */}
+			<ExamView />
+>>>>>>> d07e010 (testing)
 		</div>
 	)
 }
