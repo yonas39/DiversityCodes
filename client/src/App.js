ee5ca5c4af7edef928bcbc36c9118dd12fe2616c
Emily Flores <e_floresrodriguez@massbay.edu>
2024-02-04 20:29:45 -0500

    Resolved merge conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react"
 import "./App.css"
 import ExamView from "./pages/ExamView"
@@ -8,6 +9,26 @@ function App() {
 		<div className="App">
 			<Navbar /> {/* Render Navbar */}
 			<ExamView />
+=======
+import "./App.css"
+import { useApi } from "./hooks/use-api"
+
+function App() {
+	const { response, error } = useApi({ path: "exams" })
+
+	console.log("response:", response)
+
+	if (error) {
+		return <div>Error: {JSON.stringify(error)}</div>
+	}
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				{Array.isArray(response) &&
+					response.map((exam, index) => <p key={index}>{exam.patientId}</p>)}
+			</header>
+>>>>>>> 381f817 (fixed repo)
 		</div>
 	)
 }
