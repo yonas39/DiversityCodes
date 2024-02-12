7d5d86d0e508d1931529ffe5bb6ece4e348c1cd1
Emily Flores <110843762+efloresz@users.noreply.github.com>
2024-02-11 21:12:40 -0500

    added hacks logo to navBat + fixed layout
    revisions from prev merge

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,36 +1,38 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 import {
-	NavigationMenu,
-	NavigationMenuContent,
-	NavigationMenuItem,
-	NavigationMenuLink,
-	NavigationMenuList,
-	NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu"
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+} from "@/components/ui/navigation-menu";
 
 const NavBar = () => {
-	return (
-		<nav className="flex items-center justify-center p-6 bg-grey-500">
-			<NavigationMenu>
-				<NavigationMenuList className="space-x-4">
-					<NavigationMenuItem>
-						<Link to="/" className="text-xl text-black hover:text-blue-500">
-							Home
-						</Link>
-					</NavigationMenuItem>
-					<NavigationMenuItem>
-						<Link
-							to="/admin"
-							className="text-xl text-black hover:text-blue-300"
-						>
-							Admin
-						</Link>
-					</NavigationMenuItem>
-				</NavigationMenuList>
-			</NavigationMenu>
-		</nav>
-	)
-}
+  return (
+    <nav className="navbar">
+      <div className="logo-container">
+        <img src="/favicon.png" alt="hacksLogo" className="logo" />
+      </div>
+      <NavigationMenu>
+        <NavigationMenuList className="space-x-4">
+          <NavigationMenuItem>
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link to="/exam" className="nav-link">
+              Exams
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link to="/admin" className="nav-link">
+              Admin
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
