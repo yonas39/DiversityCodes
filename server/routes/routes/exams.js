93b444ea13d0e604da41411d79d3d88887e3209f
Emily Flores <110843762+efloresz@users.noreply.github.com>
2024-02-20 14:54:28 -0500

    deleted duplicated routes sub directory

--- server/routes/routes/exams.js
+++ server/routes/routes/exams.js
@@ -1,5 +1,3 @@
 import { getAllExams } from "../controllers/exam.js"
-import { getExamId } from "../controllers/exam.js"
 
 router.get("/exams", getAllExams)
-router.get("/exams/:examId", getExamById);
