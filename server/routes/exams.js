7752dc354c7249a68144825a226cde3535d60ee6
Emily Flores <110843762+efloresz@users.noreply.github.com>
2024-02-21 23:15:36 -0500

    created routes
    CRUD functionality

--- server/routes/exams.js
+++ server/routes/exams.js
@@ -1,3 +1,24 @@
-import { getAllExams } from "../controllers/exam.js"
+const express = require('express');
+const router = express.Router();
 
-router.get("/exams", getAllExams)
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET all exams'})
+});
+
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET a single exam'})
+});
+
+router.post('/', (req, res) => {
+    res.json({mssg: 'POST a new exam'})
+});
+
+router.delete('/:id', (req, res) => {
+    res.json({mssg: 'DELETE a exam'})
+});
+
+router.patch('/:id', (req, res) => {
+    res.json({mssg: 'UPDATE a exam'})
+});
+
+module.exports = router;
