a33f223aafa3fe9d4eb4aea9c7bf8c1564b4bbed
Emily Flores <110843762+efloresz@users.noreply.github.com>
2024-02-23 19:44:47 -0500

    added function to connect with database

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 const express = require('express');
+const mongoose = require('mongoose');
 const examRoutes = require('./routes/exams');
 
 dotenv.config();
@@ -13,6 +14,15 @@ app.use((req, res, next) => {
 
 app.use('/server/exams', examRoutes);
 
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT);
-});
+// connect to db
+mongoose.connect(process.env.DB_STRING)
+    .then(() =>{
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT);
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
