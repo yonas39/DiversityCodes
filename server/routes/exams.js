31bff0e17c62676ed070fe3003ed3c2f2a5c1f30
JoseDiazCodes <josediazjr.a@gmail.com>
2024-02-26 11:12:01 -0500

    fixed single patient exam view

--- server/routes/exams.js
+++ server/routes/exams.js
@@ -11,19 +11,13 @@ import {
 const router = express.Router()
 
 router.get("/", getAllExams)
-router.get("/:id", getExamsByPatientId)
+router.get("/patient/:patientId", getExamsByPatientId)
+router.get("/patient/:examId", getExamById)
 router.post("/", createExam)
 router.delete("/:id", deleteExam)
 router.patch("/:id", updateExam)
-router.get("/patient/:examId", getExamById)
 
 export default router
-// Define a route for getting a specific exam by its ID
-// When this file is used with '/api/exams' in app.js, this route will be '/api/exams/patient/:examId'
 
-// createExam,
-// 	getAllExams,
-// 	getExamById,
-// 	getExamsByPatientId,
-// 	deleteExam,
-// 	updateExam,
+// router.get("/exam/:id", getExamById)
+// router.get("/patient/:patientId", getExamsByPatientId)
