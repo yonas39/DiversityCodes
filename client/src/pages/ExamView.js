dd71ac6d476c336917c47a6a785f04a1f55a83e4
Yonas Mekonnen <yonasmek@mit.edu>
2024-02-25 21:27:57 -0500

    Delete button completed

--- client/src/pages/ExamView.js
+++ client/src/pages/ExamView.js
@@ -11,11 +11,33 @@ function ExamView() {
 
   const [search, setSearch] = useState("");
 
-  //TODO
-  const handleDeleteExam = (input) => {
-    console.log(alert("DELETE BUTTON CLICKED"));
+  /////////////////////////////////////////////////////
+  /////////DELETE BUTTON COMPLETED !!!!!!!!!! ////////
+  /////////////////////////////////////////////////////
+  const handleDeleteExam = async (examId) => {
+    // console.log(alert("DELETE BUTTON CLICKED"));
+    console.log("Deleting Exam with ID: ", examId); // Log the message in the console
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_BASE_URL}/server/exams/${examId}`,
+        {
+          method: "DELETE",
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`Failed to Delete Exam with ID ${examId}`);
+      }
+      // filter out the deleted exam from the state
+      setExams(exams.filter((exam) => exam._id !== examId));
+    } catch (error) {
+      console.error("Error Deleting exam: ", error);
+      setError(error.message);
+    }
   };
 
+  /////////////////////////////////////////////////////
+  /////////Update BUTTON In Progress !!!!!!!!!! ////////
+  /////////////////////////////////////////////////////
   // TODO
   const handleUpdateExam = (input) => {
     console.log(alert("EXAM UPDATE CLICKED"));
@@ -188,191 +210,3 @@ function ExamView() {
 }
 
 export default ExamView;
-
-///////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////// BELOW IS THE OLD VERSION CODE ///////////////////////////
-///////////////////////////// I KEPT IT NOT TO LOSE RELEVANT PEACE OF CODE ////////////
-///////////////////////////// CAN BE REMOVED LATER ///////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////
-
-// import React, { useEffect, useState } from "react";
-// import { Link, useNavigate } from "react-router-dom";
-
-// // import React, { useEffect, useState } from "react";
-// // import { Link, useNavigate } from "react-router-dom"; // Add Link here
-
-// function ExamView() {
-//   const [exams, setExams] = useState([]);
-//   const [search, setSearch] = useState("");
-//   const [error, setError] = useState(null);
-//   const navigate = useNavigate();
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await fetch(
-//           `${process.env.REACT_APP_API_BASE_URL}/server/exams`
-//         );
-//         if (!response.ok) {
-//           throw new Error(
-//             `Network response was not ok - Status: ${response.status}`
-//           );
-//         }
-
-//         const data = await response.json();
-//         console.log("Fetched data:", data);
-
-//         if (data.exams && Array.isArray(data.exams)) {
-//           setExams(data.exams);
-//         } else {
-//           // setError("Unexpected data format from the API");
-//           setError(
-//             `Unexpected data format from the API: ${JSON.stringify(data)}`
-//           );
-//         }
-//       } catch (error) {
-//         setError("Failed to fetch exams. Please try again later.");
-//         console.error("Error fetching exams:", error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   const handleUpdateExam = (examId) => {
-//     navigate(`/updateExam/${examId}`);
-//   };
-
-//   const handleDeleteExam = async (examId) => {
-//     try {
-//       const response = await fetch(`/server/exams/${examId}`, {
-//         method: "DELETE",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//       });
-//       if (!response.ok) {
-//         throw new Error("Failed to delete exam");
-//       }
-//       // Remove the deleted exam from the list
-//       setExams(exams.filter((exam) => exam._id !== examId));
-//     } catch (error) {
-//       console.error("Error deleting exam:", error);
-//     }
-//   };
-
-//   if (error) {
-//     return <div>Error: {error}</div>;
-//   }
-
-//   if (exams.length === 0) {
-//     return <div>Loading exams data...</div>;
-//   }
-
-//   return (
-//     <div className="admin-container bg-gray-800 text-white p-6 rounded-lg shadow-lg">
-//       <h2 className="text-2xl font-bold mb-4">Admin View</h2>
-//       <div className="flex items-center mb-4">
-// <div className="flex-grow">
-//   <label className="mr-2">Search:</label>
-//   <input
-//     type="text"
-//     className="border border-gray-600 rounded-md px-2 py-1 text-black"
-//     placeholder="Search Patient ID"
-//     value={search}
-//     onChange={(e) => setSearch(e.target.value)}
-//   />
-// </div>
-// <Link
-//   to="/admin"
-//   className="create-new-exam button bg-blue-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded"
-// >
-//   Create New Exam
-// </Link>
-//       </div>
-
-//       <table className="border-collapse border border-gray-600 w-full">
-//         <thead>
-//           <tr className="bg-gray-700">
-//             <th className="px-4 py-2">Patient ID</th>
-//             <th className="px-4 py-2">Exam ID</th>
-//             <th className="px-4 py-2">Age</th>
-//             <th className="px-4 py-2">Sex</th>
-//             <th className="px-4 py-2">Zip Code</th>
-//             <th className="px-4 py-2">BMI</th>
-//             <th className="px-4 py-2">Key Findings</th>
-//             <th className="px-4 py-2">Brixia Scores</th>
-//             <th className="px-4 py-2">Image URL</th>
-//             <th className="px-4 py-2">Update</th>
-//             <th className="px-4 py-2">Delete</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {exams
-//             .filter((exam) =>
-//               exam.patientId.toLowerCase().includes(search.trim().toLowerCase())
-//             )
-//             .map((exam, index) => (
-//               <tr key={exam._id || index} className="bg-gray-800">
-//                 <td className="border border-gray-600 px-4 py-2">
-//                   <Link
-//                     to={`/patient/${exam.patientId}`}
-//                     className="hover:underline"
-//                   >
-//                     {exam.patientId}
-//                   </Link>
-//                 </td>
-//                 <td className="border border-gray-600 px-4 py-2">
-//                   <Link to={`/exam/${exam.examId}`} className="hover:underline">
-//                     {exam.examId}
-//                   </Link>
-//                 </td>
-//                 <td className="border border-gray-600 px-4 py-2">{exam.age}</td>
-//                 <td className="border border-gray-600 px-4 py-2">{exam.sex}</td>
-//                 <td className="border border-gray-600 px-4 py-2">
-//                   {exam.zipCode}
-//                 </td>
-//                 <td className="border border-gray-600 px-4 py-2">{exam.bmi}</td>
-//                 <td className="border border-gray-600 px-4 py-2">
-//                   {exam.keyFindings}
-//                 </td>
-//                 <td className="border border-gray-600 px-4 py-2">
-//                   {exam.brixiaScores}
-//                 </td>
-//                 <td className="border border-gray-600 px-4 py-2">
-//                   <a
-//                     href={exam.imageURL}
-//                     target="_blank"
-//                     rel="noopener noreferrer"
-//                     className="text-white-400 hover:underline"
-//                   >
-//                     View Image
-//                   </a>
-//                 </td>
-// <td className="border border-gray-600 px-4 py-2">
-//   <a
-//     href="#"
-//     onClick={() => handleUpdateExam(exam._id)}
-//     className="text-blue-400 hover:underline"
-//   >
-//     Update
-//   </a>
-// </td>
-// <td className="border border-gray-600 px-4 py-2">
-//   <a
-//     href="#"
-//     onClick={() => handleDeleteExam(exam._id)}
-//     className="text-red-400 hover:underline"
-//   >
-//     Delete
-//   </a>
-// </td>
-//               </tr>
-//             ))}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// }
-
-// export default ExamView;
