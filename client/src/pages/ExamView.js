c8b9cd9b307bf3d8aaa98278fdef2b05fb3d22fb
Emily Flores <110843762+efloresz@users.noreply.github.com>
2024-02-26 16:54:16 -0500

    changed font size

--- client/src/pages/ExamView.js
+++ client/src/pages/ExamView.js
@@ -87,7 +87,7 @@ function ExamView() {
   return (
 <div className="admin-container bg-gray-800 text-white p-6 rounded-lg shadow-lg">
   <div className="border-4 border-blue-300 rounded-lg p-4 mb-4">
-    <h2 className="text-2xl font-bold mb-4">Admin View</h2>
+    <h2 className="text-3xl font-bold mb-4">Admin View</h2>
     <div className="flex-grow">
       {/* SEARCH FUNCTIONALITY */}
       <label className="mr-2">Search:</label>
