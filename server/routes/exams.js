1732527518be939d27cbcbaba8fac540d0bb7a6b
Emily Flores <110843762+efloresz@users.noreply.github.com>
2024-02-28 04:12:13 -0500

    added put route

--- server/routes/exams.js
+++ server/routes/exams.js
@@ -1,24 +1,21 @@
-import express from "express"
+import express from "express";
 import {
-	createExam,
-	getAllExams,
-	getExamById,
-	getExamsByPatientId,
-	deleteExam,
-	updateExam,
-} from "../controllers/exam.js"
+  createExam,
+  getAllExams,
+  getExamById,
+  getExamsByPatientId,
+  deleteExam,
+  updateExam,
+} from "../controllers/exam.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", getAllExams)
-router.get("/patient/:patientId", getExamsByPatientId)
-router.get("/patient/:patientId/exam/:examId", getExamById)
+router.get("/", getAllExams);
+router.get("/patient/:patientId", getExamsByPatientId);
+router.get("/patient/:patientId/exam/:examId", getExamById);
 
-router.post("/", createExam)
-router.delete("/:id", deleteExam)
-router.patch("/:id", updateExam)
-
-export default router
-
-// router.get("/exam/:id", getExamById)
-// router.get("/patient/:patientId", getExamsByPatientId)
+router.post("/", createExam);
+router.delete("/:id", deleteExam);
+//router.patch("/:id", updateExam);
+router.put("/patient/:patientId/exam/:examId", updateExam);
+export default router;
